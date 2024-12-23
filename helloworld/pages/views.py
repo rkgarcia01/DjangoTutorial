0e371992278a051ad8b3a9c6a8c3c808e6e957eb
Reyki <reykigarcia@gmail.com>
2024-12-23 16:15:59 -0600

    views.py extra comments deleted

--- helloworld/pages/views.py
+++ helloworld/pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+
 # capital HomePageView since it is class or Class-Based View(CBV)
 class HomePageView(TemplateView):
     template_name = "home.html"
@@ -14,6 +15,3 @@ class AboutPageView(TemplateView):
 
 # def homePageView(request):     # lowercase homePageView since it is a function
 # return HttpResponse("Hello, World!")
-
-
-# Create your views here.
