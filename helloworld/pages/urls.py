a6e775be2d5f6dd72493dd5742dbfaf1ed101e8f
Reyki <reykigarcia@gmail.com>
2024-12-24 16:03:24 -0600

    comments addded

--- helloworld/pages/urls.py
+++ helloworld/pages/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 from .views import HomePageView, AboutPageView
 
 # we use .as_view() when useing a Class-Based View in 'views.py'
+# where it says 'name =', this is the name of the urls in base.html it is referring to. The url tag uses these names to autmatically create links for us
 urlpatterns = [
     path("about/", AboutPageView.as_view(), name="about"),
     path("", HomePageView.as_view(), name="home"),
