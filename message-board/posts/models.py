974226de835432e5f0937e7f6c1937f25eece1a9
Reyki <reykigarcia@gmail.com>
2025-01-09 15:17:02 -0600

    comments have been added

--- message-board/posts/models.py
+++ message-board/posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-# To help us build new database models which will "model" thet characteristics of the data in our database, we import a module called "models" from django.db library
+
+# To help us build new database models which will "model" the characteristics of the data in our database, we import a module called "models" from django.db library
 # We've created a new database model called post which has the database field text.
 # We've also specified the type of content it will hold, "TextField()"
 class Post(models.Model):
--- message-board/posts/tests.py
+++ message-board/posts/tests.py
@@ -5,8 +5,10 @@ from .models import Post
 
 
 class PostTests(TestCase):
+    # ALL methods should start with the phrase "test_" so tha Django knows to test them!
     # This part checks to see if the post a user makes is equal to what is stored in the database
     @classmethod
+    # "setUptTestData()" is used to create the "test data"
     def setUpTestData(cls):
         cls.post = Post.objects.create(text="This is a test!")
 
@@ -16,6 +18,13 @@ class PostTests(TestCase):
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
+
+    # This function combines all the other three (bottom) functions in the quotations together using one response since all three  tests are really just testing that the homepage works as expected.
+    def test_homepage(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "This is a test!")
 
     """
     def test_urls_avaialbe_by_name(self):
@@ -30,6 +39,7 @@ class PostTests(TestCase):
         response = self.client.get(reverse("home"))
         self.assertContains(response, "This is a test!")
     """
+
     # This function combines all the other three functions in the quottions together using one response
     def test_homepage(self):
         response = self.client.get(reverse("home"))
