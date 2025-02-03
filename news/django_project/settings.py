d5e658209e89bbf96867c7d82feaa529e772bee3
Reyki <reykigarcia@gmail.com>
2025-02-03 11:43:39 -0600

    DEBUG (Edit)

--- news/django_project/settings.py
+++ news/django_project/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
-# DEBUG = env.bool("DEBUG", default=False)
+# DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
